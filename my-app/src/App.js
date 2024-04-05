a11be32ed48985df3fb9c2105c62b0c276871f4c
Jenkibear <jenkin.he@students.mq.edu.au>
2024-04-05 21:36:26 +1100

    tree view menu complete children recursive calls etc

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,6 +5,8 @@ import RandomColor from './components/random-color';
 import StarRating from './components/star-rating';
 import ImageSlider from './components/image-slider';
 import LoadMoreData from './components/load-more-data';
+import TreeView from './components/tree-view';
+import menus from './components/tree-view/data'
 
 function App() {
   return (
@@ -13,7 +15,8 @@ function App() {
      {/* <RandomColor></RandomColor> */}
      {/* <StarRating noOfStars={10}/> */}
      {/* <ImageSlider url={'https://picsum.photos/v2/list'} page={"1"} limit={"10"}/> */}
-     <LoadMoreData/>
+     {/* <LoadMoreData/> */}
+     <TreeView menus={menus}/>
     </div>
   );
 }
