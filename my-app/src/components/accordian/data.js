135c98631e42912da2731cf4f5315d37e52d32d1
Jenkibear <jenkin.he@students.mq.edu.au>
2024-04-05 13:43:55 +1100

    accordian

---
+++ my-app/src/components/accordian/data.js
@@ -0,0 +1,28 @@
+const data =[
+    {
+        id :'1',
+        question:"My love?",
+        answer: "Consider it"
+
+    },
+    {
+        id :'2',
+        question:"My cat?",
+        answer: "nod it"
+
+    },
+    {
+        id :'3',
+        question:"My table?",
+        answer: "make it"
+
+    },
+    {
+        id :'4',
+        question:"My charger?",
+        answer: "change it"
+
+    },
+
+];
+export default data;
