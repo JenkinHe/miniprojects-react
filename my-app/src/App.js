fa98dcc840db429a23c3fb4eb88555f1b35cb08f
Jenkibear <jenkin.he@students.mq.edu.au>
2024-04-05 18:25:51 +1100

    use api

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,6 +4,7 @@ import Accordian from './components/accordian';
 import RandomColor from './components/random-color';
 import StarRating from './components/star-rating';
 import ImageSlider from './components/image-slider';
+import LoadMoreData from './components/load-more-data';
 
 function App() {
   return (
@@ -11,7 +12,8 @@ function App() {
      {/* <Accordian></Accordian> */}
      {/* <RandomColor></RandomColor> */}
      {/* <StarRating noOfStars={10}/> */}
-     <ImageSlider url={'https://picsum.photos/v2/list'} page={"1"} limit={"10"}/>
+     {/* <ImageSlider url={'https://picsum.photos/v2/list'} page={"1"} limit={"10"}/> */}
+     <LoadMoreData/>
     </div>
   );
 }
