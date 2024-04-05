bd9f07bb21a9798e752c38d0a95b2648e89abc70
Jenkibear <jenkin.he@students.mq.edu.au>
2024-04-05 14:08:49 +1100

    single selection complete for accordian

--- my-app/src/components/accordian/index.jsx
+++ my-app/src/components/accordian/index.jsx
@@ -1,7 +1,36 @@
+// single selection
 
+import data from "./data";
+import { useState } from "react";
+import './styles.css';
+//multiple selection
 
-export default function Accordian(){
-    return <div>
-        Accordian
+export default function Accordian() {
+    const [selected, setSelected] = useState(null);
+
+    function handleSingleSelection(getCurrentId) {
+        
+        setSelected(getCurrentId===selected?null:getCurrentId);
+    }
+
+    return <div className="wrapper">
+        <button>enable multi selection</button>
+        <div className="accordian">
+            {
+                data && data.length > 0 ?
+                    data.map(dataItem => <div className="item">
+                        <div onClick={() => handleSingleSelection(dataItem.id)} className="title">
+                            <h3>{dataItem.question}</h3>
+                            <span>+</span>
+                        </div>
+                        {
+                            selected === dataItem.id ?
+                                <div className="content">{dataItem.answer}</div>
+                                : null
+                        }
+                    </div>)
+                    : <div>No Data Found!</div>
+            }
+        </div>
     </div>
 }
