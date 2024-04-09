85e41a45836361dca324fcd1a7f9152f70b66ab9
Jenkibear <jenkinwork123@gmail.com>
2024-04-10 09:33:39 +1000

    css dependent on data theme local hook store even if page reset

--- my-app/src/components/light-dark-mode/index.jsx
+++ my-app/src/components/light-dark-mode/index.jsx
@@ -10,7 +10,7 @@ export default function LightDarkMode(){
         setTheme(theme==='light'?'dark':'light');
     }
 
-    return (<div className="light-dark-mode">
+    return (<div className="light-dark-mode"data-theme={theme}>
         <div className="container">
             <p>hello world</p>
             <button onClick={handleToggleTheme}>change theme</button>
