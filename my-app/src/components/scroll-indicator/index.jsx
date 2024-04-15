06271845fe731ce9b08167f4ff40cab696edcff9
Jenkibear <jenkinwork123@gmail.com>
2024-04-15 21:56:01 +1000

    scroll percentage success

--- my-app/src/components/scroll-indicator/index.jsx
+++ my-app/src/components/scroll-indicator/index.jsx
@@ -4,6 +4,7 @@ export default function ScrollIndicator({url}){
     const [data,setData]= useState([]);
     const [loading,setLoading]= useState(false);
     const [errorMsg,setErrorMsg] = useState('');
+    const[scrollPercentage,setScrollPercentage]= useState(0);
     async function fetchData(getUrl){
         try{
             setLoading(true);
@@ -26,6 +27,23 @@ export default function ScrollIndicator({url}){
     useEffect(()=>{
         fetchData(url);
     },[url]);
+    
+    function handleScrollPercentage(){
+        console.log(document.body.scrollTop, document.documentElement.scrollTop,document.documentElement.scrollHeight,document.documentElement.clientHeight);
+        const howMuchScrolled= document.body.scrollTop||document.documentElement.scrollTop;
+        const height = document.documentElement.scrollHeight-document.documentElement.clientHeight;
+
+        setScrollPercentage((howMuchScrolled/height)*100);
+    }
+    
+
+    useEffect(()=>{
+        window.addEventListener('scroll',handleScrollPercentage)
+        return ()=>{
+            window.removeEventListener('scroll',()=>{})
+        }
+    },[]
+)
 
     return <div>
         <h1>Custom Scroll Indicator</h1>
