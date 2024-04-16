096c8492e43f26df78a1b7b4ec86ecba9fb14874
Jenkibear <jenkinwork123@gmail.com>
2024-04-16 19:58:38 +1000

    auto complete logic complete

---
+++ my-app/src/components/search-autocomplete-with-api/index.jsx
@@ -0,0 +1,66 @@
+import { useEffect, useState } from "react"
+import Suggestions from "./suggestions";
+
+
+
+
+export default function SearchAutocomplete() {
+
+    const [loading, setLoading] = useState(false);
+    const [users, setUsers] = useState([]);
+    const [error, setError] = useState(null);
+    const [searchParam, setSearchParam] = useState('');
+    const [showDropdown, setShowDropdown] = useState(false);
+    const [filteredUsers, setFilteredUsers] =useState([]);
+
+    function handleChange(event){
+        const query = event.target.value.toLowerCase();
+        setSearchParam(query);
+        if(query.length>1){
+
+            const filteredData =users&&users.length?
+            users.filter((item)=>item.toLowerCase().indexOf(query)>-1)
+            :[];
+            setFilteredUsers(filteredData);
+            setShowDropdown(true);
+        }else{
+            setShowDropdown(false);
+        }
+    }
+
+
+    async function fetchListOfUsers() {
+        try {
+            setLoading(true);
+            const response = await fetch('https://dummyjson.com/users');
+            const data = await response.json();
+
+            console.log(data);
+            if (data && data.users && data.users.length) {
+                setError(null);
+                setLoading(false);
+                setUsers(data.users.map((userItem) => userItem.firstName));
+            }
+
+        } catch (error) {
+            setLoading(false);
+            console.log(error);
+            setError(error);
+        }
+    }
+
+
+    useEffect(() => {
+        fetchListOfUsers()
+    }, [])
+
+    return <div className="search-autocomplete-container">{
+        loading?<h1>loading</h1>:
+        <input value={searchParam} name="search-users" placeholder="Search USers Here..." onChange={handleChange} />
+    }
+        
+        {   
+            showDropdown &&<Suggestions data={filteredUsers}/>
+        }
+    </div>
+}
