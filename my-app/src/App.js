b625e903419bf515c58d6522fa7a57a7ba38e4be
Jenkibear <jenkinwork123@gmail.com>
2024-04-16 17:45:09 +1000

    tabs fully functional

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -10,6 +10,7 @@ import menus from './components/tree-view/data'
 import QRCodeGenerator from './components/qr-code-generator';
 import LightDarkMode from './components/light-dark-mode';
 import ScrollIndicator from './components/scroll-indicator';
+import TabTest from './components/custom-tabs/tab-test';
 
 function App() {
   return (
@@ -23,7 +24,7 @@ function App() {
      {/* <QRCodeGenerator/> */}
      {/* <LightDarkMode/> */}
      {/* <ScrollIndicator url={'https://dummyjson.com/products?limit=100'}/> */}
-     <tabTest/>
+     <TabTest/>
 
     </div>
   );
--- my-app/src/components/custom-tabs/tabs.jsx
+++ my-app/src/components/custom-tabs/tabs.jsx
@@ -13,13 +13,13 @@ export default function Tabs({ tabsContent, onChange }) {
     return <div className="wrapper">
         <div className="heading">
             {
-                tabsContent.map(tabItem => (<div onClick={()=> handleOnClick()} key={tabItem.label}>
+                tabsContent.map((tabItem,index) => (<div onClick={()=> handleOnClick(index)} key={tabItem.label}>
                     <span className="label">{tabItem.label}</span>
                 </div>))
             }
 
         </div>
-        <div className="content">
+        <div className="content" style={{color:'red'}}>
             {
                 tabsContent[currentTabIndex] && tabsContent[currentTabIndex].content
             }
