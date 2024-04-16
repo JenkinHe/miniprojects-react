e65bb57a9a3afb7dfb7da7cf949fba5c7f32fc56
Jenkibear <jenkinwork123@gmail.com>
2024-04-16 19:24:58 +1000

    css styling for cards and format of info from api

--- my-app/src/components/github-profile-finder/index.jsx
+++ my-app/src/components/github-profile-finder/index.jsx
@@ -1,22 +1,39 @@
 import { useEffect, useState } from "react"
+import User from "./user";
+import './styles.css';
 
 
 
 export default function GithubProfileFinder(){
-    const [userName,setUserName]=useState('JenkinHe')
+    const [userName,setUserName]=useState('JenkinHe');
+    const [userData,setUserData] = useState(null);
+    const [loading,setLoading]=useState(true);
 
     function handleSubmit(){
+        fetchGithubUserData()
 
     }
 
     async function fetchGithubUserData(){
-        const response =await fetch(`https://api.github.com/users/${userName}`)
-        const data= await response.json()
+        setLoading(true)
+        const response =await fetch(`https://api.github.com/users/${userName}`);
+        const data= await response.json();
+        if(data){
+            setUserData(data);
+            setLoading(false);
+            setUserName('');
+        }
     }
 
     useEffect(()=>{
         fetchGithubUserData()
     },[])
+
+
+
+    if(loading){
+        return <h1>loading data please wait</h1>
+    }
 
     return <div className="github-profile-container">
         <div className="input-wrapper">
@@ -25,6 +42,9 @@ export default function GithubProfileFinder(){
 
             
         </div>
+        {
+            userData!==null?<User user={userData}/>:null
+        }
 
     </div>
 }
