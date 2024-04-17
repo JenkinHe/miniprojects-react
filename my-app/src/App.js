d32c17649faa5cea00a8b1977875c38bb3f62d04
Jenkibear <jenkinwork123@gmail.com>
2024-04-17 17:56:14 +1000

    window resize hook

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -19,6 +19,7 @@ import FeatureFlagGlobalState from './components/feature-flag/context';
 import FeatureFlags from './components/feature-flag';
 import UseFetchHookTest from './components/use-fetch/test';
 import UseOnclickOutsideTest from './components/use-outside-click/test';
+import UseWindowResizeTest from './components/use-window-resize/test';
 
 function App() {
   return (
@@ -41,7 +42,8 @@ function App() {
       <FeatureFlags/>
      </FeatureFlagGlobalState> */}
      {/* <UseFetchHookTest/> */}
-     <UseOnclickOutsideTest></UseOnclickOutsideTest>
+     {/* <UseOnclickOutsideTest></UseOnclickOutsideTest> */}
+     <UseWindowResizeTest/>
 
 
     </div>
---
+++ my-app/src/components/use-window-resize/test.jsx
@@ -0,0 +1,17 @@
+import useWindowResize from "."
+
+
+
+export default function UseWindowResizeTest(){
+    const windowSize =useWindowResize();
+    const {width,height} = windowSize;
+    return <div>
+        <h1>Window resize hook</h1>
+        <p>
+            width is {width}
+        </p>
+        <p>
+            height is{height}
+        </p>
+    </div>
+}
